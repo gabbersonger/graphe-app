979dc5ef80e30b8a10197f9e99160f3a380949c2
Gabriel Aravena <gabriel.alejo.gab@gmail.com>
2024-04-22 18:41:28 +1000

    Virtualiser restructure

--- frontend/src/lib/Scripture/data.ts
+++ frontend/src/lib/Scripture/data.ts
@@ -2821,7 +2821,7 @@ export const versionData = {
         book_number: 66,
         display_name: "ΑΠΟΚΑΛΥΨΙΣ ΙΩΑΝΝΟΥ",
         num_chapters: 22,
-        num_verses: [20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 19, 17, 18, 20, 8, 21, 18, 24, 21, 15, 27, 21],
+        num_verses: [20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 19, 18, 18, 20, 8, 21, 18, 24, 21, 15, 27, 21],
         parallels: [
         ],
         missing_sections: [
