88d1ceb035d17c05edb39362ff314dac314ed6e8
Gabriel Aravena <gabriel.alejo.gab@gmail.com>
2024-10-24 18:25:25 +1100

    Text displayer loads 10 chapters either side

--- frontend/src/components/Workspace/Content/TextDisplayer.svelte
+++ frontend/src/components/Workspace/Content/TextDisplayer.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import TextBlock from "@/components/Workspace/Content/TextBlock.svelte";
-
     import type { ScriptureSection } from "!/graphe/internal/data";
     import {
         ScriptureService,
@@ -8,14 +7,13 @@
     } from "!/graphe/internal/scripture";
     import { onMount, tick } from "svelte";
     import { Events } from "@wailsio/runtime";
+    import { throttle } from "@/lib/utils";
 
-    const NUM_BLOCKS_DISPLAY = 20;
-    const FULL_HEIGHT = 100_000; // This should be large
-    const BUFFER_ZONE = 10_000;
+    const NUM_BLOCK_BUFFER = 20;
+    const SCROLLABLE_HEIGHT = 100_000;
+    const SCROLLABLE_BUFFER = 10_000;
 
     let viewport: HTMLDivElement;
-    let visible_above_container: HTMLDivElement;
-    let visible_below_container: HTMLDivElement;
 
     export let data: ScriptureSection[];
     $: n_blocks = data.reduce((a, c) => a + c.blocks.length, 0) ?? 0;
@@ -33,8 +31,8 @@
     let visible_below: VirtualBlock[] = [];
     let visible_above_elements: HTMLDivElement[] = [];
     let visible_below_elements: HTMLDivElement[] = [];
-    let pivot_height = 0;
-    let mode: "exact" | "middle" | "top" | "bottom" = "top";
+    let pivot_height: number = 0;
+    let scroll_locked = false;
 
     function reset() {
         current_verse = undefined;
@@ -43,14 +41,89 @@
         visible_above_elements.length = 0;
         visible_below_elements.length = 0;
         pivot_height = 0;
-        mode = "top";
+        scroll_locked = false;
     }
 
-    // Called when new data is loaded
     function load() {
         reset();
-        current_verse = data[0].blocks[0].verses[0].ref;
-        displayAroundPivot(0);
+        loadVirtualBlock({ index: 0, section: 0, block: 0 });
+    }
+
+    function refresh() {
+        if (scroll_locked) return;
+        if (n_blocks == 0) return;
+
+        const scroll = viewport.scrollTop;
+        const { pivot, offset, remaining } = calculatePivot(scroll);
+
+        if (pivot.index == visible_below[0].index) return;
+        throttled_update(pivot, offset, remaining);
+    }
+    const throttled_refresh = throttle(refresh, 300);
+
+    async function update(vb: VirtualBlock, offset: number, remaining: number) {
+        // Update current verse
+        const new_block = data[vb.section].blocks[vb.block];
+        current_verse = new_block.verses[0].ref;
+
+        // Handle reaching the start of blocks
+        if (vb.index == 0) return loadVirtualBlock(vb, remaining);
+
+        // Handle reaching the end of blocks
+        if (vb.index + NUM_BLOCK_BUFFER >= n_blocks)
+            return loadVirtualBlock(vb, remaining);
+
+        // Handle getting too close to edge while in the middle
+        if (
+            pivot_height + offset < SCROLLABLE_BUFFER ||
+            pivot_height + offset > SCROLLABLE_HEIGHT - SCROLLABLE_BUFFER
+        ) {
+            return loadVirtualBlock(vb, remaining);
+        }
+
+        pivot_height += offset;
+        updateViritualBlocks(vb.index);
+    }
+    const throttled_update = throttle(update, 300);
+
+    function calculatePivot(scroll: number): {
+        pivot: VirtualBlock;
+        offset: number;
+        remaining: number;
+    } {
+        let pivot: VirtualBlock;
+        let offset = 0;
+        let remaining = scroll - pivot_height;
+
+        if (remaining > 0) {
+            const last = visible_below_elements.findLastIndex((e) => e != null);
+            let i = 0;
+            while (remaining > 0 && i < last) {
+                const height = visible_below_elements[i].clientHeight;
+                if (remaining - height > 0) {
+                    remaining -= height;
+                    offset += height;
+                } else break;
+                i++;
+            }
+            pivot = visible_below[i];
+        } else {
+            let i = visible_above_elements.findLastIndex((e) => e != null);
+            while (remaining < 0 && i >= 0) {
+                const height = visible_above_elements[i].clientHeight;
+                remaining += height;
+                offset -= height;
+                if (remaining >= 0) break;
+                i--;
+            }
+            pivot = i < 0 ? visible_below[0] : visible_above[i];
+        }
+
+        return {
+            pivot: pivot,
+            offset: offset,
+            remaining: remaining,
+        };
     }
 
     function getVirtualBlocks(
@@ -58,7 +131,7 @@
         start: number,
         length: number | undefined = undefined,
     ) {
-        if (length == undefined) length = NUM_BLOCKS_DISPLAY;
+        if (length == undefined) length = NUM_BLOCK_BUFFER;
         if (start < 0 || start >= n_blocks || length <= 0) {
             output.length = 0;
             return output;
@@ -100,103 +173,54 @@
         return output;
     }
 
-    function getVirtualBlocksAbove(output: VirtualBlock[], pivot: number) {
-        const start = Math.max(pivot - NUM_BLOCKS_DISPLAY, 0);
-        const length = pivot - start;
-        return getVirtualBlocks(output, start, length);
-    }
-
-    async function refresh() {
-        if (n_blocks == 0) return;
-        let scroll = viewport.scrollTop;
-
-        let vae_length = 0;
-        for (; vae_length < visible_above_elements.length; vae_length++)
-            if (visible_above_elements[vae_length] == null) break;
-
-        // Get the new pivot and the offset within that new pivot
-        let new_pivot_offset = 0;
-        let offset_remaining = 0;
-        if (scroll > pivot_height) {
-            offset_remaining = scroll - pivot_height;
-            for (let i = 0; i < visible_below_elements.length; i++) {
-                const height = visible_below_elements[i].clientHeight;
-                if (offset_remaining - height > 0) {
-                    offset_remaining -= height;
-                } else {
-                    new_pivot_offset = i;
-                    break;
-                }
-            }
-        } else {
-            offset_remaining *= -1;
-            for (let i = 1; i <= vae_length; i++) {
-                offset_remaining -=
-                    visible_above_elements[vae_length - i].clientHeight;
-                if (offset_remaining <= 0) {
-                    new_pivot_offset = -i;
-                    break;
-                }
-            }
-        }
-
-        if (new_pivot_offset == 0) return;
-
-        // Update current verse
-        let new_virtual: VirtualBlock;
-        if (new_pivot_offset > 0) {
-            new_virtual = visible_below[new_pivot_offset];
-        } else {
-            new_virtual =
-                visible_above[visible_above.length + new_pivot_offset];
-        }
-        const new_block = data[new_virtual.section].blocks[new_virtual.block];
-        current_verse = new_block.verses[0].ref;
-        // FIX: currently this is a very approximate current verse
-        // (does not take into account offset_remaining)
-
-        // Update pivot height
-        let moved_height = 0;
-        if (new_pivot_offset > 0) {
-            for (let i = 0; i < new_pivot_offset; i++)
-                moved_height += visible_below_elements[i].clientHeight;
-        } else {
-            for (let i = 1; i <= -new_pivot_offset; i++)
-                moved_height -=
-                    visible_above_elements[vae_length - i].clientHeight;
-        }
-        pivot_height += moved_height;
-
-        // Update data being displayed
-        const new_pivot = visible_below[0].index + new_pivot_offset;
-        displayAroundPivot(new_pivot);
-    }
-
-    async function displayAroundPivot(pivot: number) {
-        visible_above = getVirtualBlocksAbove(visible_above, pivot);
+    function updateViritualBlocks(pivot: number) {
+        const start = Math.max(pivot - NUM_BLOCK_BUFFER, 0);
+        visible_above = getVirtualBlocks(visible_above, start, pivot - start);
         visible_below = getVirtualBlocks(visible_below, pivot);
-
-        const top_reached =
-            visible_above.length > 0 && visible_above[0].index == 0;
-        const bottom_reached =
-            visible_below.length > 0 &&
-            visible_below[visible_below.length - 1].index == n_blocks - 1;
-
-        if (top_reached && bottom_reached) {
-            mode = "exact";
-        } else if (top_reached) {
-            mode = "top";
-        } else if (bottom_reached) {
-            mode = "bottom";
-        } else {
-            mode = "middle";
-        }
     }
 
-    function goto(block: number) {
-        pivot_height = FULL_HEIGHT / 2;
-        displayAroundPivot(block);
-        viewport.scrollTop = pivot_height;
+    async function loadVirtualBlock(vb: VirtualBlock, offset: number = 0) {
+        // Stop re-loading if it is the current block already loaded
+        if (visible_below.length > 0 && visible_below[0].index == vb.index)
+            return;
+
+        // Stop re-loading bottom if bottom already loaded
+        if (
+            visible_below.length > 0 &&
+            visible_below[0].index < vb.index &&
+            visible_below[visible_below.length - 1].index == n_blocks - 1
+        )
+            return;
+
+        updateViritualBlocks(vb.index);
+        current_verse = data[vb.section].blocks[vb.block].verses[0].ref;
+
+        if (visible_below.length > 0 && visible_below[0].index == 0) {
+            // At the top
+            pivot_height = 0;
+        } else if (
+            visible_below.length > 0 &&
+            visible_below[visible_below.length - 1].index == n_blocks - 1
+        ) {
+            // At the bottom
+            pivot_height = SCROLLABLE_HEIGHT;
+            scroll_locked = true;
+            await tick();
+        } else {
+            // In the middle
+            pivot_height = SCROLLABLE_HEIGHT / 2;
+        }
+
+        // Scroll to the new content
+        const current_scroll = viewport ? viewport.scrollTop : 0;
+        if (pivot_height == current_scroll) return;
+
+        scroll_locked = true;
+        viewport.scrollTop = pivot_height + offset;
+        setTimeout(() => {
+            scroll_locked = false;
+        }, 10);
+        // FIX: this relies on 10ms being enough time for scrollTop to finish
     }
 
     async function gotoRef(ref: ScriptureRef) {
@@ -214,7 +238,11 @@
                         ref,
                     );
                     if (block_contains) {
-                        return goto(nth_item);
+                        loadVirtualBlock({
+                            index: nth_item,
+                            section: i,
+                            block: j,
+                        });
                     }
                     nth_item += 1;
                 }
@@ -222,12 +250,10 @@
         }
     }
 
-    function handleEvent(event_data: any) {
-        gotoRef(event_data.data as ScriptureRef);
-    }
-
     onMount(() => {
-        Events.On("window:workspace:text:goto", handleEvent);
+        Events.On("window:workspace:text:goto", (event_data: any) => {
+            gotoRef(event_data.data as ScriptureRef);
+        });
         return () => {
             Events.Off("window:workspace:text:goto");
         };
@@ -235,24 +261,16 @@
 </script>
 
 <div class="container">
-    <div class="viewport" bind:this={viewport} on:scroll={refresh}>
+    <div class="viewport" bind:this={viewport} on:scroll={throttled_refresh}>
         <div class="content">
             <div
                 class="rows"
-                data-mode={mode}
-                style:--full-height="{FULL_HEIGHT}px"
-                style:--pivot-height="{pivot_height}px"
+                style:min-height={`${SCROLLABLE_HEIGHT}px`}
+                style:--pivot-height={`${pivot_height}px`}
             >
-                <div
-                    class="collection"
-                    bind:this={visible_above_container}
-                    style:bottom={`calc(100% - ${pivot_height}px)`}
-                >
+                <div class="collection above">
                     {#each visible_above as vb, index (vb.index)}
-                        <div
-                            bind:this={visible_above_elements[index]}
-                            style="outline: 2px solid red;"
-                        >
+                        <div bind:this={visible_above_elements[index]}>
                             <TextBlock
                                 block={data[vb.section].blocks[vb.block]}
                             />
@@ -260,16 +278,9 @@
                     {/each}
                 </div>
 
-                <div
-                    class="collection"
-                    bind:this={visible_below_container}
-                    style:top={`${pivot_height}px`}
-                >
+                <div class="collection below">
                     {#each visible_below as vb, index (vb.index)}
-                        <div
-                            bind:this={visible_below_elements[index]}
-                            style="outline: 2px solid blue;"
-                        >
+                        <div bind:this={visible_below_elements[index]}>
                             <TextBlock
                                 block={data[vb.section].blocks[vb.block]}
                             />
@@ -294,9 +305,9 @@
         overflow-y: scroll;
     }
 
-    /* .viewport::-webkit-scrollbar {
+    .viewport::-webkit-scrollbar {
         display: none;
-    } */
+    }
 
     .content {
         position: relative;
@@ -311,27 +322,15 @@
     }
 
     .collection {
+        position: absolute;
         width: 100%;
     }
 
-    .rows[data-mode="exact"] {
-        height: unset;
+    .collection.above {
+        bottom: calc(100% - var(--pivot-height));
     }
 
-    .rows[data-mode="exact"],
-    .rows[data-mode="top"] {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-    }
-
-    .rows[data-mode="middle"] .collection {
-        position: absolute;
-    }
-
-    .rows[data-mode="bottom"] {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
+    .collection.below {
+        top: var(--pivot-height);
     }
 </style>
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -15,3 +15,45 @@ export function GrapheLog(severity: "info" | "error", message: string) {
       throw Error(message);
   }
 }
+
+export function throttle<R, A extends any[]>(
+  fn: (...args: A) => R,
+  delay: number,
+): (...args: A) => R | undefined {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  return (...args: A) => {
+    if (!timeout) {
+      const val = fn(...args);
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, delay);
+      return val;
+    }
+  };
+}
+
+// export function throttle<F extends Function>(
+//   fn: (args: T) => any,
+//   delay: number,
+// ) {
+//   let timeout: ReturnType<typeof setTimeout> | null = null;
+//   return (...args: Parameters<T>) => {
+//     if (!timeout) {
+//       fn(...args);
+//       timeout = setTimeout(() => {
+//         timeout = null;
+//       }, delay);
+//     }
+//   };
+// }
+// export function throttle(fn: Function, delay: number) {
+//   let timeout: ReturnType<typeof setTimeout> | null = null;
+//   return (...args: any[]) => {
+//     if (!timeout) {
+//       fn(...args);
+//       timeout = setTimeout(() => {
+//         timeout = null;
+//       }, delay);
+//     }
+//   };
+// }
